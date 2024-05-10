88eb2ba4d86a05a7b272140a00f23888b1b44ce9
inmiti <inmabauvel@gmail.com>
2024-05-10 09:08:43 +0200

    Initial Commit

---
+++ PropertiesWrapped/PropertiesWrappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertiesWrappedApp.swift
+//  PropertiesWrapped
+//
+//  Created by ibautista on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PropertiesWrappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
